86f517cacffb9c7d49efafb29ddfc9bf5e2423f4
Jasper Kent <jasper_kent@hotmail.com>
2025-02-25 09:46:11 +0000

    Add project files.

---
+++ BlazorPrerendering/BlazorPrerendering.Client/Models/Book.cs
@@ -0,0 +1,16 @@
+﻿namespace BlazorPrerendering.Models;
+
+public class Book
+{
+    public int Id { get; set; }
+    public required string Title { get; set; }
+    public int Year { get; set; }
+
+    public static List<Book> Books { get; } =
+    [
+        new () {Id = 1, Title = "Emma", Year = 1816 },
+            new () {Id = 2, Title = "Mansfield Park", Year = 1814 },
+            new () {Id = 3, Title = "Dr No", Year = 1958 },
+            new () {Id = 4, Title = "Goldfinger", Year = 1959 }
+    ];
+}
---
+++ BlazorPrerendering/BlazorPrerendering/Program.cs
@@ -0,0 +1,39 @@
+using BlazorPrerendering.Components;
+using BlazorPrerendering.Data;
+using BlazorPrerendering.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7135") });
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(BlazorPrerendering.Client._Imports).Assembly);
+
+app.MapGet("/api/Books", () => DummyData.Books);
+
+app.Run();
